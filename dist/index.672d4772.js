e8a222224992a527a9d4f604fc74d3de3ea79b0a
Adefolami <akintokunadefolami@gmail.com>
2024-07-04 12:31:55 +0100

    deployment

---
+++ dist/index.672d4772.js
@@ -0,0 +1,5 @@
+"use strict";
+const button = document.querySelector(".btn");
+button.addEventListener("click", prompt("congrats"));
+
+//# sourceMappingURL=index.672d4772.js.map
---
+++ dist/index.77215b82.js
@@ -0,0 +1,2 @@
+document.querySelector(".btn").addEventListener("click",prompt("congrats"));
+//# sourceMappingURL=index.77215b82.js.map
